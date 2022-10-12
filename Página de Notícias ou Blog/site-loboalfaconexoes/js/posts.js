61fe81c1a89cd242a0e4c37c15966f0ab11274bd
Robson-Brasil <Robson.Brasil@outlook.com>
2022-10-12 13:06:16 -0400

    Incluindo nosvos projetos de WebSite
    Signed-off-by: Robson-Brasil <Robson.Brasil@outlook.com>

---
+++ Página de Notícias ou Blog/site-loboalfaconexoes/js/posts.js
@@ -0,0 +1,122 @@
+var posts = [
+  {
+    imagem: "https://tm.ibxk.com.br/2021/12/21/21150534978340.jpg?ims=1120x420",
+    titulo: "Internet das Coisas",
+    autor: "Robson Brasil",
+    data: "11 Out 2022",
+    imagem_post:
+      "http://hmbtecnologia.com.br/wp-content/uploads/2021/08/iot.jpeg",
+    texto:
+      "A Internet das Coisas - IoT - é o processo no qual qualquer pessoa ou organização pode conectar itens físicos cotidianos a uma rede. Isto varia desde os objetos domésticos mais comuns, como um refrigerador ou uma máquina de café, até dispositivos maciços, como semáforos, máquinas pesadas ou até mesmo um drone projetado para espionagem ou segurança de um país. Neste post, você vai aprender tudo sobre a Internet das Coisas, inclusive: Conceito Importância Como funciona    Tecnologias que utiliza    Aplicações reais    Benefícios    O que é IoT    E muito mais!    Se você está interessado em aprender como os objetos que não estavam tradicionalmente conectados podem agora ser conectados graças à transformação digital e como usar esta tecnologia para obter melhores insights, processos e ações dentro de uma organização, leia até o fim!    O que é a Internet das Coisas?    A Internet das Coisas, ou IoT para abreviar seu termo em inglês Internet of Things, refere-se à interconexão que existe através de uma rede de alvos físicos - coisas - confiando na tecnologia fornecida principalmente por sensores, software e outras. O objetivo é partilhar dados uns com os outros e trocar informações para fins de eficiência.    De acordo com um relatório feito por Statista, existem mais de 35,8 bilhões de dispositivos IoT conectados no mundo em 2021 e este número poderia mais que dobrar para 75,4 bilhões até 2025.Isto significa que, em média, todas as pessoas no mundo têm 4,4 dispositivos IoT conectados - com base em uma densidade populacional de 8 bilhões de pessoas. Isto indica claramente que, mais do que uma tendência, a Internet das Coisas é uma regra para a rotina diária de bilhões de seres humanos. Importância de IoT Se o número de dispositivos IoT atingiu estes níveis em 2021, é claro que esta tecnologia se tornou rapidamente uma das mais difundidas no século 21, e talvez na história da humanidade.Neste momento, pessoas em todo o planeta - de diferentes níveis sociais, países ou alcance econômico - podem fazer uso da IoT. Pense nisso, hoje quase todos possuem um telefone celular, aparelho ou gadget que está conectado à Internet, até mesmo monges no Tibete se comunicam com o mundo exterior através da Internet!",
+  },
+  {
+    imagem: "https://picsum.photos/id/200/348/185",
+    titulo: "Um título 2",
+    autor: "José Otavio",
+    data: "02 Fev 2019",
+    imagem_post: "https://picsum.photos/id/200/1110/590",
+    texto:
+      "2 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a convallis tellus. Maecenas vel velit nec urna auctor iaculis vitae ut neque. Integer mollis eros vestibulum lacus accumsan, non vestibulum odio hendrerit. Sed volutpat sem vitae libero commodo, aliquet convallis tellus viverra. Nulla molestie faucibus erat sit amet semper. In hac habitasse platea dictumst. Donec eget neque non elit lobortis efficitur. Nunc vitae neque sollicitudin enim faucibus mattis ut ut tellus.",
+    video: "https://www.youtube.com/embed/y__ero7suPo",
+  },
+  {
+    imagem: "https://picsum.photos/id/300/348/185",
+    titulo: "Um título 3",
+    autor: "José Otavio",
+    data: "02 Fev 2019",
+    imagem_post: "https://picsum.photos/id/300/1110/590",
+    texto:
+      "3 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a convallis tellus. Maecenas vel velit nec urna auctor iaculis vitae ut neque. Integer mollis eros vestibulum lacus accumsan, non vestibulum odio hendrerit. Sed volutpat sem vitae libero commodo, aliquet convallis tellus viverra. Nulla molestie faucibus erat sit amet semper. In hac habitasse platea dictumst. Donec eget neque non elit lobortis efficitur. Nunc vitae neque sollicitudin enim faucibus mattis ut ut tellus.",
+    video: "https://www.youtube.com/embed/vvt2hgREqag",
+  },
+  {
+    imagem: "https://picsum.photos/id/400/348/185",
+    titulo: "Um título 4",
+    autor: "José Otavio",
+    data: "02 Fev 2019",
+    imagem_post: "https://picsum.photos/id/400/1110/590",
+    texto:
+      "4 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a convallis tellus. Maecenas vel velit nec urna auctor iaculis vitae ut neque. Integer mollis eros vestibulum lacus accumsan, non vestibulum odio hendrerit. Sed volutpat sem vitae libero commodo, aliquet convallis tellus viverra. Nulla molestie faucibus erat sit amet semper. In hac habitasse platea dictumst. Donec eget neque non elit lobortis efficitur. Nunc vitae neque sollicitudin enim faucibus mattis ut ut tellus.",
+    video: "https://www.youtube.com/embed/yIO_7K4-JjU",
+  },
+  {
+    imagem: "https://picsum.photos/id/55/348/185",
+    titulo: "Um título 5",
+    autor: "José Otavio",
+    data: "02 Fev 2019",
+    imagem_post: "https://picsum.photos/id/55/1110/590",
+    texto:
+      "5 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a convallis tellus. Maecenas vel velit nec urna auctor iaculis vitae ut neque. Integer mollis eros vestibulum lacus accumsan, non vestibulum odio hendrerit. Sed volutpat sem vitae libero commodo, aliquet convallis tellus viverra. Nulla molestie faucibus erat sit amet semper. In hac habitasse platea dictumst. Donec eget neque non elit lobortis efficitur. Nunc vitae neque sollicitudin enim faucibus mattis ut ut tellus.",
+    video: "https://www.youtube.com/embed/yIO_7K4-JjU",
+  },
+  {
+    imagem: "https://picsum.photos/id/99/348/185",
+    titulo: "Um título 5",
+    autor: "José Otavio",
+    data: "02 Fev 2019",
+    imagem_post: "https://picsum.photos/id/99/1110/590",
+    texto:
+      "6 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a convallis tellus. Maecenas vel velit nec urna auctor iaculis vitae ut neque. Integer mollis eros vestibulum lacus accumsan, non vestibulum odio hendrerit. Sed volutpat sem vitae libero commodo, aliquet convallis tellus viverra. Nulla molestie faucibus erat sit amet semper. In hac habitasse platea dictumst. Donec eget neque non elit lobortis efficitur. Nunc vitae neque sollicitudin enim faucibus mattis ut ut tellus.",
+    video: "https://www.youtube.com/embed/yIO_7K4-JjU",
+  },
+  {
+    imagem: "https://picsum.photos/id/73/348/185",
+    titulo: "Um título 5",
+    autor: "José Otavio",
+    data: "02 Fev 2019",
+    imagem_post: "https://picsum.photos/id/73/1110/590",
+    texto:
+      "7 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a convallis tellus. Maecenas vel velit nec urna auctor iaculis vitae ut neque. Integer mollis eros vestibulum lacus accumsan, non vestibulum odio hendrerit. Sed volutpat sem vitae libero commodo, aliquet convallis tellus viverra. Nulla molestie faucibus erat sit amet semper. In hac habitasse platea dictumst. Donec eget neque non elit lobortis efficitur. Nunc vitae neque sollicitudin enim faucibus mattis ut ut tellus.",
+    video: "https://www.youtube.com/embed/yIO_7K4-JjU",
+  },
+  {
+    imagem: "https://picsum.photos/id/85/348/185",
+    titulo: "Um título 5",
+    autor: "José Otavio",
+    data: "02 Fev 2019",
+    imagem_post: "https://picsum.photos/id/85/1110/590",
+    texto:
+      "8 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a convallis tellus. Maecenas vel velit nec urna auctor iaculis vitae ut neque. Integer mollis eros vestibulum lacus accumsan, non vestibulum odio hendrerit. Sed volutpat sem vitae libero commodo, aliquet convallis tellus viverra. Nulla molestie faucibus erat sit amet semper. In hac habitasse platea dictumst. Donec eget neque non elit lobortis efficitur. Nunc vitae neque sollicitudin enim faucibus mattis ut ut tellus.",
+    video: "https://www.youtube.com/embed/yIO_7K4-JjU",
+  },
+  {
+    imagem: "https://picsum.photos/id/91/348/185",
+    titulo: "Um título 5",
+    autor: "José Otavio",
+    data: "02 Fev 2019",
+    imagem_post: "https://picsum.photos/id/91/1110/590",
+    texto:
+      "9 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a convallis tellus. Maecenas vel velit nec urna auctor iaculis vitae ut neque. Integer mollis eros vestibulum lacus accumsan, non vestibulum odio hendrerit. Sed volutpat sem vitae libero commodo, aliquet convallis tellus viverra. Nulla molestie faucibus erat sit amet semper. In hac habitasse platea dictumst. Donec eget neque non elit lobortis efficitur. Nunc vitae neque sollicitudin enim faucibus mattis ut ut tellus.",
+    video: "https://www.youtube.com/embed/yIO_7K4-JjU",
+  },
+  {
+    imagem: "https://picsum.photos/id/100/348/185",
+    titulo: "Um título 5",
+    autor: "José Otavio",
+    data: "02 Fev 2019",
+    imagem_post: "https://picsum.photos/id/103/1110/590",
+    texto:
+      "10 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a convallis tellus. Maecenas vel velit nec urna auctor iaculis vitae ut neque. Integer mollis eros vestibulum lacus accumsan, non vestibulum odio hendrerit. Sed volutpat sem vitae libero commodo, aliquet convallis tellus viverra. Nulla molestie faucibus erat sit amet semper. In hac habitasse platea dictumst. Donec eget neque non elit lobortis efficitur. Nunc vitae neque sollicitudin enim faucibus mattis ut ut tellus.",
+    video: "https://www.youtube.com/embed/yIO_7K4-JjU",
+  },
+  {
+    imagem: "https://picsum.photos/id/111/348/185",
+    titulo: "Um título 5",
+    autor: "José Otavio",
+    data: "02 Fev 2019",
+    imagem_post: "https://picsum.photos/id/111/1110/590",
+    texto:
+      "11 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a convallis tellus. Maecenas vel velit nec urna auctor iaculis vitae ut neque. Integer mollis eros vestibulum lacus accumsan, non vestibulum odio hendrerit. Sed volutpat sem vitae libero commodo, aliquet convallis tellus viverra. Nulla molestie faucibus erat sit amet semper. In hac habitasse platea dictumst. Donec eget neque non elit lobortis efficitur. Nunc vitae neque sollicitudin enim faucibus mattis ut ut tellus.",
+    video: "https://www.youtube.com/embed/yIO_7K4-JjU",
+  },
+  {
+    imagem: "https://picsum.photos/id/12/348/185",
+    titulo: "Um título 5",
+    autor: "José Otavio",
+    data: "02 Fev 2019",
+    imagem_post: "https://picsum.photos/id/12/1110/590",
+    texto:
+      "12 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a convallis tellus. Maecenas vel velit nec urna auctor iaculis vitae ut neque. Integer mollis eros vestibulum lacus accumsan, non vestibulum odio hendrerit. Sed volutpat sem vitae libero commodo, aliquet convallis tellus viverra. Nulla molestie faucibus erat sit amet semper. In hac habitasse platea dictumst. Donec eget neque non elit lobortis efficitur. Nunc vitae neque sollicitudin enim faucibus mattis ut ut tellus.",
+    video: "https://www.youtube.com/embed/yIO_7K4-JjU",
+  },
+];
