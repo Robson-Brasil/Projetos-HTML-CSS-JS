9f804db330d7c3dd8f8f2d138168c31a5f61a972
Robson-Brasil <Robson.Brasil@outlook.com>
2022-10-12 21:33:27 -0400

    delete 15 files and update 1 file
    Signed-off-by: Robson-Brasil <Robson.Brasil@outlook.com>

--- Página de Notícias ou Blog/site-loboalfaconexoes/js/home.js
+++ Página de Notícias ou Blog/site-loboalfaconexoes/js/home.js
@@ -49,33 +49,33 @@ var mySwiperPosts = new Swiper(".swiper-container-posts", {
     // when window width is >= 320px
     320: {
       slidesPerView: 1,
-      spaceBetween: 30,
+      spaceBetween: 20,
     },
 
     // when window width is >= 480px
     768: {
       slidesPerView: 2,
-      spaceBetween: 30,
+      spaceBetween: 20,
     },
     // when window width is >= 980px
     980: {
       slidesPerView: 3,
-      spaceBetween: 30,
+      spaceBetween: 20,
     },
     320: {
       slidesPerView: 4,
-      spaceBetween: 30,
+      spaceBetween: 20,
     },
 
     // when window width is >= 480px
     768: {
       slidesPerView: 5,
-      spaceBetween: 30,
+      spaceBetween: 20,
     },
     // when window width is >= 980px
     980: {
       slidesPerView: 6,
-      spaceBetween: 30,
+      spaceBetween: 20,
     },
   },
 });
